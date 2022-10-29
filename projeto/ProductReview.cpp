ede69ad1a6823aa9ea99317b91c02905c2dc7c68
IsaCristine <bell2011jf@gmail.com>
2022-10-29 16:15:19 -0300

    Inicializa ideia de implementação dos métodos da ProductReview.cpp

--- projeto/ProductReview.cpp
+++ projeto/ProductReview.cpp
@@ -1,14 +1,34 @@
 #include <iostream>
+#include <fstream>
 #include "ProductReview.h"
 
 using namespace std;
 
+ProductReview::ProductReview(int registro)
+{
+    //inicializa variável para ler o arquivo:
+    ifstream arq("ratings_Electronics.bin", ios::binary);
 
-ProductReview::ProductReview()
-{}
+    //verifica se o arquivo foi aberto:
+    if (!arq.is_open())
+    {
+        cout << "Erro ao abrir o arquivo!" << endl;
+        return;
+    }
+
+    //posiciona o cursor no início do registro
+
+    //lê o registro
+}
 
 ProductReview::~ProductReview()
 {}
 
 void ProductReview::print()
-{}
+{
+    //imprime o conteúdo do registro do arquivo binário:
+    cout << "User ID: " << this->userId << endl;
+    cout << "Product ID: " << this->productId << endl;
+    cout << "Rating: " << this->rating << endl;
+    cout << "Timestamp: " << this->timestamp << endl;
+}
