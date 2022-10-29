8f64dde2196c3b41eb110dea186db095e664b62e
IsaCristine <bell2011jf@gmail.com>
2022-10-29 15:04:09 -0300

    Cria métodos da source ProducReview

--- projeto/ProductReview.cpp
+++ projeto/ProductReview.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 #include "ProductReview.h"
+
+using namespace std;
+
+
+ProductReview::ProductReview()
+{}
+
+ProductReview::~ProductReview()
+{}
+
+void ProductReview::print()
+{}
