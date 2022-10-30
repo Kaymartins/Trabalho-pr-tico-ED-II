b9064efa486e5aabcec40ed9840093ce2b14344b
IsaCristine <bell2011jf@gmail.com>
2022-10-29 21:09:50 -0300

    Inicializa desenvolvimento em grupo da Etapa 1

--- projeto/Main.cpp
+++ projeto/Main.cpp
@@ -1,10 +1,30 @@
-//
-// Created by Kayan on 23/10/2022.
-//
-
 #include <iostream>
+#include <fstream>
 
 using namespace std;
+
+
+void createBinary(string& path)
+{
+    //cria arquivo binário:
+    FILE *arq;
+
+    arq = fopen(path.c_str(),);
+    fstream arq(path,ios::out | ios::binary);
+    int vet[100];
+    int resultado;
+    while (!arq.eof())
+    {
+        new ProductReview  = fread(&vet[0], sizeof(int), 100, arq);
+    }
+    //lê o arquivo até o fim e escreve no arquivo binário:
+    while(!arq.eof())
+    {
+        
+    }
+
+    arq.close();
+}
 
 int main()
 {
