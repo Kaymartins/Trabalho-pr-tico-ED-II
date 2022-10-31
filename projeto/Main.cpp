74c6f9bbb02d86f776086e095b442e63929733e8
IsaCristine <bell2011jf@gmail.com>
2022-10-31 17:52:01 -0300

    Atualiza ideia de implementação do getReview

--- projeto/Main.cpp
+++ projeto/Main.cpp
@@ -4,70 +4,69 @@
 
 using namespace std;
 
-void createBinary(string& path)
+void createBinary(string &path)
 {
-    //abre o arquivo de texto no diretório path:
+    // abre o arquivo de texto no diretório path:
     ifstream arq(path);
 
-    //cria vetor de objetos ProductReview:
+    // cria vetor de objetos ProductReview:
     int tam = 100;
     ProductReview *reviews = new ProductReview[tam];
 
-    if(!arq.is_open())
+    if (!arq.is_open())
     {
         cout << "ERRO: Erro ao abrir o arquivo!" << endl;
         return;
     }
 
-    //lê o arquivo de texto e armazena no vetor de objetos:
+    // lê o arquivo de texto e armazena no vetor de objetos:
     int i = 0;
-    while(!arq.eof())
+    while (!arq.eof())
     {
         if (i == tam)
         {
             ampliaVetor(reviews, &tam);
         }
-        
+
         string userId, productId, strRating, timestamp;
         float rating;
 
-        //lê cada linha do arquivo de texto e armazena os dados em uma string:
+        // lê cada linha do arquivo de texto e armazena os dados em uma string:
         getline(arq, userId, ',');
         getline(arq, productId, ',');
         getline(arq, strRating, ',');
         getline(arq, timestamp, '\n');
 
-        //seta os dados do objeto para ProductReview:
+        // seta os dados do objeto para ProductReview:
         reviews[i].setUserId(userId);
         reviews[i].setProductId(productId);
         rating = stof(strRating);
         reviews[i].setRating(rating);
         reviews[i].setTimestamp(timestamp);
-        
-        //atualiza o contador:
+
+        // atualiza o contador:
         i++;
     }
 
-    //fecha o arquivo de texto:
+    // fecha o arquivo de texto:
     arq.close();
 
-    //abre o arquivo binário:
+    // abre o arquivo binário:
     ofstream arqBin("path/ratings_Electronics.bin", ios::binary);
 
-    if(!arqBin.is_open())
+    if (!arqBin.is_open())
     {
         cout << "ERRO: Erro ao abrir o arquivo!" << endl;
         return;
     }
 
-    //escreve o arquivo binário:
-    for(i = 0; i < 100; i++)
+    // escreve o arquivo binário:
+    for (i = 0; i < 100; i++)
     {
-        if(i == tam - 1)
-        arqBin.write((char*)&reviews[i], sizeof(ProductReview));
+        if (i == tam - 1)
+            arqBin.write((char *)&reviews[i], sizeof(ProductReview));
     }
 }
-
 
 void ampliaVetor(ProductReview *vetor, int *tamanho)
 {
@@ -81,38 +80,50 @@ void ampliaVetor(ProductReview *vetor, int *tamanho)
     *tamanho = (*tamanho) * 2;
 }
 
-
 void getReview(int i)
 {
-    //abre o arquivo binário para leitura:
+    // abre o arquivo binário para leitura:
     ifstream arqBin("path/ratings_Electronics.bin", ios::binary);
 
-    if(!arqBin.is_open())
+    if (!arqBin.is_open())
     {
         cout << "ERRO: Erro ao abrir o arquivo!" << endl;
         return;
     }
 
-    //posiciona o cursor no registro i:
+    // posiciona o cursor no registro i:
     arqBin.seekg(i * sizeof(ProductReview));
 
-    //lê o registro i:
+    // inicializa variável do tipo ProductReview:
     ProductReview review;
-    arqBin.read((char*)&review, sizeof(ProductReview));
 
-    //fecha o arquivo binário:
+    // inicializa variáveis de dados auxiliares:
+    string userId, productId, strRating, timestamp;
+    float rating;
+
+    // lê cada linha do arquivo de texto e armazena os dados em uma string:
+    getline(arqBin, userId, ',');
+    getline(arqBin, productId, ',');
+    getline(arqBin, strRating, ',');
+    getline(arqBin, timestamp, '\n');
+
+    // seta os dados do objeto para ProductReview:
+    review.setUserId(userId);
+    review.setProductId(productId);
+    rating = stof(strRating);
+    review.setRating(rating);
+    review.setTimestamp(timestamp);
+
+    // fecha o arquivo binário:
     arqBin.close();
 
-    //imprime o conteúdo do registro:
+    // imprime o conteúdo do registro:
     review.print();
 }
 
-
 int main()
 {
-    cout<<"Hello World";
+    cout << "Hello World";
 
     return 0;
 }
-
-
