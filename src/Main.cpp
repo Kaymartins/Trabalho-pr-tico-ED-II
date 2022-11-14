e8cd032e159ceace94b71f5df2966672635b8504
fredericodgv <fredericodgv@gmail.com>
2022-11-13 22:20:04 -0300

    implementa contagem de tempo para os algoritmos de ordenacao

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,8 +3,11 @@
 //
 
 #include <iostream>
+#include <ctime>
+#include <chrono>
 
 using namespace std;
+using namespace chrono;
 
 int main()
 {
@@ -18,28 +21,38 @@ int main()
     if(sortOrHash == 1){
         int M = 3; //quantidade de cojutos de dados para analise
         int N[5] = {10000, 50000, 100000, 500000, 1000000}; //quantidade de dados para analise
-        float tempo[3]; //vetor para armazenar o tempo de execução de cada algoritmo
+        double tempo[3]; //vetor para armazenar o tempo de execução de cada algoritmo
 
 
-        for(int i = 0; i < M; i++){ //executa o algoritmo de mergeSort para cada conjunto de dados
-            tempo[0] += mergeSort(vet[i]);            
+        for(int i = 0; i < M; i++){ //executa o algoritmo de mergeSort para cada conjunto de 
+            high_resolution_clock::time_point inicio = high_resolution_clock::now();
+            //mergeSort(/*parametro*/);            
+            high_resolution_clock::time_point fim = high_resolution_clock::now();
+            tempo[0] += duration_cast<duration<double>>(fim - inicio).count();
         }
         tempo[0] /= M; //calcula a média do tempo de execução do algoritmo de mergeSort
 
         for(int i = 0; i < M; i++){ //executa o algoritmo de quickSort para cada conjunto de dados
-            tempo[1] += quickSort(vet[i]);            
+            high_resolution_clock::time_point inicio = high_resolution_clock::now();
+            //quickSort(vet[i]);
+            high_resolution_clock::time_point fim = high_resolution_clock::now();
+            tempo[1] += duration_cast<duration<double>>(fim - inicio).count();
         }
         tempo[1] /= M; //calcula a média do tempo de execução do algoritmo de quickSort
 
         for(int i = 0; i < M; i++){ //executa o algoritmo de countSort para cada conjunto de dados
-            tempo[2] += countSort(vet[i]);            
+            high_resolution_clock::time_point inicio = high_resolution_clock::now();
+            //countSort(vet[i]);            
+
+            high_resolution_clock::time_point fim = high_resolution_clock::now();
+            tempo[2] += duration_cast<duration<double>>(fim - inicio).count();
         }
         tempo[2] /= M; //calcula a média do tempo de execução do algoritmo de countSort
 
         //imprime os resultados
-        cout << "Tempo medio de execucao do algoritmo de mergeSort: " << tempo[0] << endl;
-        cout << "Tempo medio de execucao do algoritmo de quickSort: " << tempo[1] << endl;
-        cout << "Tempo medio de execucao do algoritmo de countSort: " << tempo[2] << endl;
+        cout << "Tempo medio de execucao do algoritmo de mergeSort: " << tempo[0] << " segundos." << endl;
+        cout << "Tempo medio de execucao do algoritmo de quickSort: " << tempo[1] << " segundos." << endl;
+        cout << "Tempo medio de execucao do algoritmo de countSort: " << tempo[2] << " segundos." << endl;
     }
     else if(sortOrHash == 2){
         //Eecuta a etapa de tabelaHash de Produtos mais avaliados
