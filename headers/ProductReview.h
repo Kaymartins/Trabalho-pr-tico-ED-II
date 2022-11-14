e999880cec2fe4da67939d99982d2b5bdc98e54e
fredericodgv <fredericodgv@gmail.com>
2022-11-13 22:25:03 -0300

    atualiza header de ProductReview com métodos necessários

--- headers/ProductReview.h
+++ headers/ProductReview.h
@@ -1,18 +1,28 @@
 #ifndef PRODUCTREVIEW_H_INCLUDED
 #define PRODUCTREVIEW_H_INCLUDED
 
-#include <string>;
+#include <string>
 
 using namespace std;
 
-class ProductReview {
+class ProductReview{
     private:
         string userId;
-        int productId;
+        string productId;
         float rating;
-        int timestamp;
+        string timestamp;
     public:
-        void ProductReview::print();
+        ProductReview();
+        ~ProductReview();
+        void setUserId(string userId){this->userId = userId;};
+        void setProductId(string productId){this->productId = productId;};
+        void setRating(float rating){this->rating = rating;};
+        void setTimestamp(string timestamp){this->timestamp = timestamp;};
+        string getUserId(){return this->userId;};
+        string getProductId(){return this->productId;};
+        float getRating(){return this->rating;};
+        string getTimestamp(){return this->timestamp;};
+        void print();
 };
 
 #endif
