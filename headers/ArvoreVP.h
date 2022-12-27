4d67d3afa657fbd77d26d320a72af98c0f7ca51d
Kayan <kayanmartins1@gmail.com>
2022-12-27 12:53:49 -0300

    cria header da arvore vermelho-preto

---
+++ headers/ArvoreVP.h
@@ -0,0 +1,37 @@
+#ifndef ARVOREVP_H_INCLUDED
+#define ARVOREVP_H_INCLUDED
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+enum Cor {VERMELHO, PRETO};
+
+struct No{
+    int data;
+    Cor cor;
+    No *esq, *dir, *pai;
+
+    No(int data) : data(data){
+        esq = dir = pai = NULL;
+        cor = VERMELHO;
+    }
+};
+
+class ArvoreVP{
+    private:
+        No *raiz; 
+    
+    protected:
+        void rotacaoEsquerda(No *&, No *&);
+        void rotacaoDireita(No *&, No *&);
+        void balancearInsercao(No *&, No *&);
+    public:
+        ArvoreVP() : raiz(NULL){}
+        void inserir(int data);
+        void imprimir();
+};
+
+
+#endif
